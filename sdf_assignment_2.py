d933f534366666edd9b54e006b9c2926e9087d46
Gagan <gkaur21.rrc.ca>
2024-09-19 14:49:19 -0500

    Initial commit: Set up project directory and file structure

---
+++ sdf_assignment_2.py
@@ -0,0 +1,20 @@
+"""
+Description: This program demonstrates different kinds of collections in Python.
+Author: Gaganpreet Kaur
+Date: September,19,2024
+
+
+"""
+
+#SIMPLE DATA TYPES
+
+#CALCULATIONS
+
+#LISTS
+
+#TUPLES
+
+#DICTIONARIES
+
+#SETS
+
